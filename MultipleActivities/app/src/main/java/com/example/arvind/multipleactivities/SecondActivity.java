ed5b591e2e20309c986a32c453642c32e9b6182e
arvind-iyer <arvindi.me@gmail.com>
2015-11-27 21:42:04 +0800

    Multiple Activities

---
+++ MultipleActivities/app/src/main/java/com/example/arvind/multipleactivities/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.arvind.multipleactivities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+    private static String value = "myValue";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Toast.makeText(this, value, Toast.LENGTH_LONG).show();
+    }
+
+}
